cadadd77168d05ff214933bf4aa9ef9b91fc9958
odaly <cruzodaly25@gmail.com>
2026-04-18 22:33:49 -0600

    optimizacion

---
+++ src/test/java/sv/edu/udb/desafio_practico_2_cf252175/DesafioPractico2Cf252175ApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafio_practico_2_cf252175;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPractico2Cf252175ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
